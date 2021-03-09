bff68c138f634033ee7fd4cdb528da9e5c6e323c
yuanxiwang <837230933@qq.com>
2021-03-09 18:27:24 +0800

    init

--- app/src/main/java/cn/e_xiaofu/opengldemo/util/ShaderHelper.kt
+++ app/src/main/java/cn/e_xiaofu/opengldemo/util/ShaderHelper.kt
@@ -1,6 +1,7 @@
 package cn.e_xiaofu.opengldemo.util
 
 import android.opengl.GLES20
+import android.util.Log
 
 /**
  * @author 袁希望
@@ -8,6 +9,7 @@ import android.opengl.GLES20
  * description:// TODO 2021/3/9 17:51
  */
 object ShaderHelper {
+    val TAG = "ShaderHelper"
     fun compileVertexShader(vertexShaderCode:String):Int{
         return compileShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
     }
@@ -17,7 +19,21 @@ object ShaderHelper {
     }
 
     fun compileShader(type:Int, shaderCode:String):Int{
-
-        return 0
+        val glCreateShader = GLES20.glCreateShader(type)
+        if (glCreateShader == 0) {
+            Log.d(TAG, "Could not create new shader.")
+            return 0
+        }
+        GLES20.glShaderSource(glCreateShader, shaderCode)
+        GLES20.glCompileShader(glCreateShader)
+        var compileState = IntArray(1)
+        GLES20.glGetShaderiv(glCreateShader, GLES20.GL_COMPILE_STATUS, compileState, 0)
+        Log.d(TAG, GLES20.glGetShaderInfoLog(glCreateShader))
+        if (compileState[0] == 0) {
+            GLES20.glDeleteShader(glCreateShader)
+            Log.d(TAG, "Compilation of shader failed.")
+            return 0
+        }
+        return glCreateShader
     }
 }
